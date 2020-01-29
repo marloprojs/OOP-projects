767728ac83eee0a7636e0c08687998be906e5c3b
mate6483 <mate6483@colorado.edu>
2020-01-28 22:41:10 -0700

    reeeeeee

---
+++ Animal.java
@@ -0,0 +1,121 @@
+import java.io.*;
+import java.util.Random;
+
+public class Animal
+{
+	public Animal()
+	{
+		System.out.println("REEEEEE");
+	}
+
+	public void sleep()
+	{
+		System.out.println("Sleeping");
+	}
+	public int random_number_generator()
+	{
+		Random rand = new Random();
+		int num = rand.nextInt(2);
+		return num;
+	}
+}
+
+class Feline extends Animal
+{
+	public void roam();
+	{
+		System.out.println("Wandering");
+	}
+}
+
+class Cat extends Feline
+{
+	public void random_action_generator()
+	{
+		int num = random_number_generator();
+		if(num == 0)
+		{
+			roam();
+		}
+		else
+		{
+			sleep();
+		}
+	}
+	public void makeNoise()
+	{
+		random_action_generator();
+	}
+}
+
+class Lion extends Feline
+{
+	public void makeNoise()
+	{
+		System.out.println("Roar");
+	}
+}
+
+class Tiger extends Feline
+{
+	public void makeNoise()
+	{
+		System.out.println("Grrrrrr");
+	}
+}
+
+class Canine extends Animal
+{
+	public void roam()
+	{
+		System.out.println("Scampering");
+	}
+}
+
+class Wolf extends Canine
+{
+	public void makeNoise()
+	{
+		System.out.println("Grrrrr");
+	}
+}
+
+class Dog extends Canine
+{
+	public void makeNoise()
+	{
+		System.out.println("Bark Bark");
+	}
+}
+
+class Pachyderm extends Animal 
+{
+	public void roam()
+	{
+		System.out.println("Stompping");
+	}
+}
+
+class Hippo extends Pachyderm
+{
+	public void makeNoise()
+	{
+		System.out.println("Whaaaa");
+	}
+}
+
+class Rhino extends Pachyderm
+{
+	public void makeNoise()
+	{
+		System.out.println("Grumble Grumble");
+	}
+}
+
+class Elephant extends Pachyderm
+{
+	public void makeNoise()
+	{
+		System.out.println("Raaaaaaa");
+	}
+}
