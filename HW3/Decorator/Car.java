935bfd873635c302bbc0218a68e3c20f36314c39
mate6483 <mate6483@colorado.edu>
2020-02-18 19:28:23 -0700

    Basic layout for hw3

---
+++ HW3/Decorator/Car.java
@@ -0,0 +1,32 @@
+import java.io.*;
+
+//Create Car class as the Component of the decorator
+public class Car
+{
+    public String licenseID = ""
+}
+
+class Economy extends Car
+{
+    public int pricePerDay = 
+}
+
+class Standard extends Car
+{
+    public int pricePerDay = 
+}
+
+class Luxury extends Car
+{
+    public int pricePerDay = 
+}
+
+class Suv extends Car
+{
+    public int pricePerDay = 
+}
+
+class Minivan extends Car
+{
+    public int pricePerDay = 
+}
