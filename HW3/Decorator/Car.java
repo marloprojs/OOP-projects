ad208354b0744d55c33ecfcb12c35bfdc172d8db
mate6483 <mate6483@colorado.edu>
2020-02-18 20:02:41 -0700

    Decorator

--- HW3/Decorator/Car.java
+++ HW3/Decorator/Car.java
@@ -3,30 +3,35 @@ import java.io.*;
 //Create Car class as the Component of the decorator
 public class Car
 {
-    public String licenseID = ""
+    public String features = ""
+    public String licenseID = "";
+    public void getFeatures(this)
+    {
+        return self.features;
+    }
 }
 
 class Economy extends Car
 {
-    public int pricePerDay = 
+    public int pricePerDay = 20;
 }
 
 class Standard extends Car
 {
-    public int pricePerDay = 
+    public int pricePerDay = 30;
 }
 
 class Luxury extends Car
 {
-    public int pricePerDay = 
+    public int pricePerDay = 45;
 }
 
 class Suv extends Car
 {
-    public int pricePerDay = 
+    public int pricePerDay = 35;
 }
 
 class Minivan extends Car
 {
-    public int pricePerDay = 
+    public int pricePerDay = 25;
 }
---
+++ HW3/Decorator/Decorator.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+//Create Car class as the Component of the decorator
+public class Decorator
+{
+    public void getFeatures(this)
+    {
+        System.out.println("");
+    }
+}
